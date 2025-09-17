a81e7ae85d4901267a7682136d72a5c294e78846
Styleiconsai_ <saikumarmsk7799@gmail.com>
2025-09-17 14:29:24 +0530

    Update HelloWorld.java

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -1,1 +1,23 @@
-public class HelloWorld { public static void main(String[] args) { System.out.println("Hello CI/CD!"); } }
+package com.example.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, World! This is my first Servlet running on Tomcat 9</h1>");
+        out.println("</body></html>");
+    }
+}
